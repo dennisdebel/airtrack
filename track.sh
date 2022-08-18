7983a455a6cf79b7aa71426b6759046fcfa1c056
dennisdebel <dennisdebel@gmail.com>
2022-08-18 21:57:10 +0200

    Update track.sh

--- track.sh
+++ track.sh
@@ -3,4 +3,4 @@
 open /System/Applications/FindMy.app;
 osascript -e 'tell application "Finder"' -e 'set visible of process "Find My" to false' -e 'end tell';
 sleep 10; 
-/usr/local/bin/jq -r '.[] | select(.name == "0003") | .location | "\(.latitude) \(.longitude) \(.timeStamp)"' /Users/s/Library/Caches/com.apple.findmy.fmipcore/Items.data >> /Users/s/Library/Scripts/log.txt
+/usr/local/bin/jq -r '.[] | select(.name == "AIRTAGNAME") | .location | "\(.latitude) \(.longitude) \(.timeStamp)"' /Users/YOURUSERNAME/Library/Caches/com.apple.findmy.fmipcore/Items.data >> /Users/YOURUSERNAME/Library/Scripts/log.txt
