fef36e7455a739a5abb88fc187f30986ef1a7bcc
dennisdebel <dennisdebel@gmail.com>
2023-12-21 17:47:44 +0800

    Update track.sh
    removed the need for putting your username in the script

--- track.sh
+++ track.sh
@@ -3,4 +3,4 @@
 open /System/Applications/FindMy.app;
 osascript -e 'tell application "Finder"' -e 'set visible of process "Find My" to false' -e 'end tell';
 sleep 10; 
-/usr/local/bin/jq -r '.[] | select(.name == "AIRTAGNAME") | .location | "\(.latitude) \(.longitude) \(.timeStamp)"' /Users/YOURUSERNAME/Library/Caches/com.apple.findmy.fmipcore/Items.data >> /Users/YOURUSERNAME/Library/Scripts/log.txt
+/usr/local/bin/jq -r '.[] | select(.name == "AIRTAGNAME") | .location | "\(.latitude) \(.longitude) \(.timeStamp)"' $HOME/Library/Caches/com.apple.findmy.fmipcore/Items.data >> $HOME/Library/Scripts/log.txt
